42e5e979eea871a21594892366ffaaa71cee461f
Sebastian Iorga <si@globalwaterintel.com>
2014-06-17 15:53:27 +0100

    remove some unneeded string manipulation

--- lib/front_axle/application_helper.rb
+++ lib/front_axle/application_helper.rb
@@ -76,7 +76,7 @@ module FrontAxle
       results.facets[f_name]['terms'].each do |t|
         item = ''.html_safe
         item += check_box_tag "q[#{f_name}][]", t['term'], param ? param.member?(t['term']) : nil
-        item += t['term'].present? ? t['term'].humanize.to_s.split.map(&:capitalize).join(' ') : 'Not specified'
+        item += t['term'].present? ? t['term'].humanize : 'Not specified'
         item += " (#{t['count']})"
         out += content_tag :div, item, class: 'facet-line'
       end
